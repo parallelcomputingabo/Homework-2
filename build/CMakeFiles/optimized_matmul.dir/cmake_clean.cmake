064353ab11e4e21bdaecba12a09612e598183f33
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2025-05-07 21:47:31 +0300

    md-aslam-hossain: Implemented optimized matrix multiplication

---
+++ build/CMakeFiles/optimized_matmul.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optimized_matmul.dir/src/main.cpp.o"
+  "CMakeFiles/optimized_matmul.dir/src/main.cpp.o.d"
+  "optimized_matmul"
+  "optimized_matmul.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/optimized_matmul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
